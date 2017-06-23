e96a9915f4b86a67691c870e1804bbcd3f48656f
huxiang <908811048@qq.com>
2017-06-23 17:32:19 +0800

    commit Web_Rent project

---
+++ public/Web_Rent/asset/scripts/areas/manage/operate/manage_view.js
@@ -0,0 +1,14 @@
+/**
+ * 辅导资料
+ * @param  {[type]} $           [description]
+ * @param  {[type]} ko          [description]
+ * @param  {[type]} common      [description]
+ * @param  {Object} xhr)        {               var ajax [description]
+ * @return {[type]}             [description]
+ */
+define(['common', 'utils/xhr', 'utils/copyutil','jquery' ], function( common, xhr, ClipBoard,$) {
+
+    $(function() {
+        
+     })
+});
